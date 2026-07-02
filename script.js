7708567a4e832c9a5309e196d6223bf91bae82e6
村松遥 <>
2026-07-02 23:20:26 +0900

    スタート画面

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function startGame() {
+    document.getElementById("title-screen").style.display = "none";
+    document.getElementById("game-screen").style.display = "block";
+
+    resetGame();
+}
 const boardElement = document.getElementById("board");
 const statusElement = document.getElementById("status");
 
